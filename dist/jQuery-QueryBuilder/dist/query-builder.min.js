d976e4ecb94177809570af534e457eb8629e5767
mistic100 <mistic100@users.noreply.github.com>
2014-06-11 15:12:17 +0200

    update doc template

--- dist/jQuery-QueryBuilder/dist/query-builder.min.js
+++ dist/jQuery-QueryBuilder/dist/query-builder.min.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery QueryBuilder 1.0.0
+ * jQuery QueryBuilder 1.1.0
  * Copyright 2014 Damien "Mistic" Sorel (http://www.strangeplanet.fr)
  * Licensed under MIT (http://opensource.org/licenses/MIT)
  */
