0c47d65fea8668d877e5791a2ad885f178745244
mistic100 <contact@git.strangeplanet.fr>
2014-08-26 10:30:28 +0200

    Closes #26 no answer from OP, manually merged + UTF-8

---
+++ dist/i18n/da.js
@@ -0,0 +1,35 @@
+/*!
+ * jQuery QueryBuilder
+ * Oversat af Jna Borup Coyle, github@coyle.dk
+ */
+
+jQuery.fn.queryBuilder.defaults.set({ lang: {
+  "add_rule": "Tilføj regel",
+  "add_group": "Tilføj gruppe",
+  "delete_rule": "Slet regel",
+  "delete_group": "Slet gruppe",
+
+  "condition_and": "OG",
+  "condition_or": "ELLER",
+
+  "filter_select_placeholder": "------",
+
+  "operator_equal": "lig med",
+  "operator_not_equal": "ikke lige med",
+  "operator_in": "i",
+  "operator_not_in": "ikke i",
+  "operator_less": "mindre",
+  "operator_less_or_equal": "mindre eller lig med",
+  "operator_greater": "større",
+  "operator_greater_or_equal": "større eller lig med",
+  "operator_begins_with": "begynder med",
+  "operator_not_begins_with": "begynder ikke med",
+  "operator_contains": "indeholder",
+  "operator_not_contains": "indeholder ikke",
+  "operator_ends_with": "slutter med",
+  "operator_not_ends_with": "slutter ikke med",
+  "operator_is_empty": "er tom",
+  "operator_is_not_empty": "er ikke tom",
+  "operator_is_null": "er null",
+  "operator_is_not_null": "er ikke null"
+}});
