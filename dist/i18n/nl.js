8d385b2dd04a527a0def39b8eaf34e28a0a0b67f
Roywcm <info@roywulms.nl>
2014-11-13 10:13:33 +0100

    Added dutch translation

---
+++ dist/i18n/nl.js
@@ -0,0 +1,36 @@
+/*!
+ * jQuery QueryBuilder
+ * Reference language file
+ */
+
+jQuery.fn.queryBuilder.defaults.set({ lang: {
+  "add_rule": "Nieuwe regel",
+  "add_group": "Nieuwe groep",
+  "delete_rule": "Verwijder",
+  "delete_group": "Verwijder",
+
+  "condition_and": "EN",
+  "condition_or": "OF",
+
+  "filter_select_placeholder": "------",
+
+  "operator_equal": "gelijk",
+  "operator_not_equal": "niet gelijk",
+  "operator_in": "in",
+  "operator_not_in": "niet in",
+  "operator_less": "minder",
+  "operator_less_or_equal": "minder of gelijk",
+  "operator_greater": "groter",
+  "operator_greater_or_equal": "groter of gelijk",
+  "operator_between": "tussen",
+  "operator_begins_with": "begint met",
+  "operator_not_begins_with": "begint niet met",
+  "operator_contains": "bevat",
+  "operator_not_contains": "bevat niet",
+  "operator_ends_with": "eindigt met",
+  "operator_not_ends_with": "eindigt niet mer",
+  "operator_is_empty": "is leeg",
+  "operator_is_not_empty": "is niet leeg",
+  "operator_is_null": "is null",
+  "operator_is_not_null": "is niet null"
+}});
