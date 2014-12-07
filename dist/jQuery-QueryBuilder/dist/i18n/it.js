b1b39275fb6982d84faeeeb4ad3d5f3d327e2024
mistic100 <mistic100@users.noreply.github.com>
2014-12-07 12:18:26 +0100

    update resources

---
+++ dist/jQuery-QueryBuilder/dist/i18n/it.js
@@ -0,0 +1,32 @@
+/*!
+ * jQuery QueryBuilder
+ * italian translation
+ */
+
+jQuery.fn.queryBuilder.defaults.set({ lang: {
+  "add_rule": "Aggiungi regola",
+  "add_group": "Aggiungi gruppo",
+  "delete_rule": "Elimina",
+  "delete_group": "Elimina",
+  "condition_and": "E",
+  "condition_or": "O",
+  "filter_select_placeholder": "------",
+  "operator_equal": "uguale",
+  "operator_not_equal": "non uguale",
+  "operator_in": "in",
+  "operator_not_in": "non in",
+  "operator_less": "minore",
+  "operator_less_or_equal": "minore o uguale",
+  "operator_greater": "maggiore",
+  "operator_greater_or_equal": "maggiore o uguale",
+  "operator_begins_with": "inizia con",
+  "operator_not_begins_with": "non inizia con",
+  "operator_contains": "contiene",
+  "operator_not_contains": "non contiene",
+  "operator_ends_with": "finisce con",
+  "operator_not_ends_with": "non finisce con",
+  "operator_is_empty": "è vuoto",
+  "operator_is_not_empty": "non è vuoto",
+  "operator_is_null": "è nullo",
+  "operator_is_not_null": "non è nullo"
+}});
