8d25ac85c3dd8df28863fdca81e37a591ed787d2
mistic100 <mistic@strangeplanet.fr>
2015-03-14 11:06:38 +0100

    New plugin *bt-checbox* using awesome-bootstrap-checkbox

---
+++ src/plugins/bt-checkbox/plugin.js
@@ -0,0 +1,55 @@
+/*jshint multistr:true */
+
+QueryBuilder.define('bt-checkbox', function(options) {
+
+    options = $.extend({
+        font: 'glyphicons',
+        color: 'default'
+    }, options);
+
+    if (options.font == 'glyphicons') {
+        var injectCSS = document.createElement('style');
+        injectCSS.innerHTML = '\
+.checkbox input[type=checkbox]:checked + label:after { \
+    font-family: "Glyphicons Halflings"; \
+    content: "\\e013"; \
+} \
+.checkbox label:after { \
+    padding-left: 4px; \
+    padding-top: 2px; \
+    font-size: 9px; \
+}';
+        document.body.appendChild(injectCSS);
+    }
+
+    this.on('getRuleInput', function(h, rule, name) {
+        var filter = rule.filter;
+
+        if ((filter.input === 'radio' || filter.input === 'checkbox') && !filter.plugin) {
+            h = '';
+
+            if (!filter.colors) {
+                filter.colors = {};
+            }
+            else if (typeof filter.colors == 'string') {
+                filter.colors = { _def: filter.colors };
+            }
+
+            var style = filter.vertical ? ' style="display:block"' : '',
+                i = 0, color, id;
+
+            iterateOptions(filter.values, function(key, val) {
+                color = filter.colors[key] || filter.colors._def || options.color;
+                id = name +'_'+ (i++);
+
+                h+= '\
+<div'+ style +' class="'+ filter.input +' '+ filter.input +'-'+ color +'"> \
+  <input type="'+ filter.input +'" name="'+ name +'" id="'+ id +'" value="'+ key +'"> \
+  <label for="'+ id +'">'+ val +'</label> \
+</div>';
+            });
+        }
+
+        return h;
+    });
+});
