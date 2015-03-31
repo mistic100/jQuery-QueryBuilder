240c62bc6601dfd51687fea60fa11cca68d57500
Olav Øyen <olav@netflex.no>
2015-03-31 15:21:16 +0200

    Adding norwegian language file

---
+++ dist/i18n/no.js
@@ -0,0 +1,33 @@
+/*!
+ * jQuery QueryBuilder 1.4.2
+ * Oversat af Jna Borup Coyle, github@coyle.dk
+ * Licensed under MIT (http://opensource.org/licenses/MIT)
+ */
+jQuery.fn.queryBuilder.defaults({ lang: {
+  "add_rule": "Legg til regel",
+  "add_group": "Legg til gruppe",
+  "delete_rule": "Slett regel",
+  "delete_group": "Slett gruppe",
+  "condition_and": "OG",
+  "condition_or": "ELLER",
+  "operators": {
+    "equal": "er lik",
+    "not_equal": "er ikke lik",
+    "in": "finnes i",
+    "not_in": "finnes ikke i",
+    "less": "er mindre enn",
+    "less_or_equal": "er mindre eller lik",
+    "greater": "er større enn",
+    "greater_or_equal": "er større eller lik",
+    "begins_with": "begynner med",
+    "not_begins_with": "begynner ikke med",
+    "contains": "inneholder",
+    "not_contains": "inneholder ikke",
+    "ends_with": "slutter med",
+    "not_ends_with": "slutter ikke med",
+    "is_empty": "er tom",
+    "is_not_empty": "er ikke tom",
+    "is_null": "er null",
+    "is_not_null": "er ikke null"
+  }
+}});
