d552fde68147ec6a8f654be409a51155af2e4171
Damien SOREL <damien.sorel@sogelink.fr>
2017-05-17 08:58:02 +0200

    Fix #487 Temporary fix CommonJS loader

--- src/.wrapper.js
+++ src/.wrapper.js
@@ -1,6 +1,9 @@
 (function(root, factory) {
     if (typeof define == 'function' && define.amd) {
-        define(['jquery', 'doT', 'jQuery.extendext'], factory);
+        define(['jquery', 'dot/doT', 'jquery-extendext'], factory);
+    }
+    else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require('jquery'), require('dot/doT'), require('jquery-extendext'));
     }
     else {
         factory(root.jQuery, root.doT);
@@ -9,5 +12,7 @@
 "use strict";
 
 @@js
+
+return QueryBuilder;
 
 }));
