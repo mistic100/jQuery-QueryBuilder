4881f28e7f4414d0cba0effde097d72a18b6ff07
Jacques Vincilione <jvincilione@gmail.com>
2017-07-24 14:51:46 -0400

    Remove missed copy/paste artifact

--- src/plugins/collapse-groups/plugin.js
+++ src/plugins/collapse-groups/plugin.js
@@ -30,36 +30,6 @@ QueryBuilder.define('collapse-groups', function(options) {
     iconDown: 'glyphicon glyphicon-chevron-down'
 });
 
-QueryBuilder.defaults({
-    operatorOpposites: {
-        'equal':            'not_equal',
-        'not_equal':        'equal',
-        'in':               'not_in',
-        'not_in':           'in',
-        'less':             'greater_or_equal',
-        'less_or_equal':    'greater',
-        'greater':          'less_or_equal',
-        'greater_or_equal': 'less',
-        'between':          'not_between',
-        'not_between':      'between',
-        'begins_with':      'not_begins_with',
-        'not_begins_with':  'begins_with',
-        'contains':         'not_contains',
-        'not_contains':     'contains',
-        'ends_with':        'not_ends_with',
-        'not_ends_with':    'ends_with',
-        'is_empty':         'is_not_empty',
-        'is_not_empty':     'is_empty',
-        'is_null':          'is_not_null',
-        'is_not_null':      'is_null'
-    },
-
-    conditionOpposites: {
-        'AND': 'OR',
-        'OR': 'AND'
-    }
-});
-
 QueryBuilder.extend({
     /**
      * Collapse a group
