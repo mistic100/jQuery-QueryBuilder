9648d307cef1b2874c26a95e9d9d9b3fd2001ce5
premithk <premith@gmail.com>
2018-02-09 18:25:26 +0530

    Checking for bootstrap select plugin and exiting

--- src/plugins/chosen-selectpicker/plugin.js
+++ src/plugins/chosen-selectpicker/plugin.js
@@ -6,15 +6,25 @@
  * @throws MissingLibraryError
  */
 QueryBuilder.define('chosen-selectpicker', function(options) {
+
+    //Checking if bootstrap select is already chosen as a plugin
+    if($.fn.selectpicker){
+        console.warn('Plugin Conflict', 'Bootstrap select is already selected as a plugin. chosen-selectpicker plugin is exiting');
+        return;
+    }
+
     if (!$.fn.chosen) {
         Utils.error('MissingLibrary', ' chosen is required to use "chosen-selectpicker" plugin. Get it here: https://github.com/harvesthq/chosen');
     }
 
     var Selectors = QueryBuilder.selectors;
-    
+    console.log('Selectors->', QueryBuilder.selectors);    
+    console.log('Selectors->', QueryBuilder.plugins);    
     // init selectpicker
     this.on('afterCreateRuleFilters', function(e, rule) {
         rule.$el.find(Selectors.rule_filter).removeClass('form-control').chosen();
+        console.log(rule.$el.find(Selectors.rule_filter).children());
+        console.log(rule.$el);
     });
 
     this.on('afterCreateRuleOperators', function(e, rule) {
