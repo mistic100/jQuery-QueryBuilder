e7dcf7a5fea6e720eb4bfb69d9133e81cb8877c8
premithk <premith@gmail.com>
2018-02-13 12:23:50 +0530

    Code cleanup, WIP

--- src/plugins/chosen-selectpicker/plugin.js
+++ src/plugins/chosen-selectpicker/plugin.js
@@ -7,42 +7,42 @@
  */
 QueryBuilder.define('chosen-selectpicker', function(options) {
 
-    //Checking if bootstrap select is already chosen as a plugin
-    if($.fn.selectpicker){
-        console.warn('Plugin Conflict', 'Bootstrap select is already selected as a plugin. chosen-selectpicker plugin is exiting');
-        return;
-    }
-
     if (!$.fn.chosen) {
         Utils.error('MissingLibrary', ' chosen is required to use "chosen-selectpicker" plugin. Get it here: https://github.com/harvesthq/chosen');
     }
 
-    var Selectors = QueryBuilder.selectors;
-    console.log('Selectors->', QueryBuilder.selectors);    
-    console.log('Selectors->', QueryBuilder.plugins);    
     // init selectpicker
     this.on('afterCreateRuleFilters', function(e, rule) {
+        if($(".form-control").length === 0 || $(".form-control").prop('nodeName') !== 'SELECT')     
+            Utils.error('MissingLibrary', ' chosen is required to use "chosen-selectpicker" plugin. Get it here: https://github.com/harvesthq/chosen');
         rule.$el.find(Selectors.rule_filter).removeClass('form-control').chosen();
-        console.log(rule.$el.find(Selectors.rule_filter).children());
-        console.log(rule.$el);
     });
 
     this.on('afterCreateRuleOperators', function(e, rule) {
+        console.log("Length Insidec", $(".form-control").length);
+        if($(".form-control").length !== 0 && $(".form-control").prop('nodeName') === 'SELECT')    
         rule.$el.find(Selectors.rule_operator).removeClass('form-control').chosen();
     });
 
     // update selectpicker on change
     this.on('afterUpdateRuleFilter', function(e, rule) {
+        console.log("Length Insideuf", $(".form-control").length);
+        if($(".form-control").length !== 0 && $(".form-control").prop('nodeName') === 'SELECT')    
         rule.$el.find(Selectors.rule_filter).trigger("chosen:updated");
     });
 
     this.on('afterUpdateRuleOperator', function(e, rule) {
+        console.log("Length Insideu",$(".form-control").length);
+        if($(".form-control").length !== 0 && $(".form-control").prop('nodeName') === 'SELECT')    
         rule.$el.find(Selectors.rule_operator).trigger("chosen:updated");
 
     });
 
     this.on('beforeDeleteRule', function(e, rule) {
-        rule.$el.find(Selectors.rule_filter).chosen('destroy');
-        rule.$el.find(Selectors.rule_operator).chosen('destroy');
+        console.log("Length Insidea", $(".form-control").length);
+        if($(".form-control").length !== 0 && $(".form-control").prop('nodeName') === 'SELECT') {    
+            rule.$el.find(Selectors.rule_filter).chosen('destroy');
+            rule.$el.find(Selectors.rule_operator).chosen('destroy');
+        }
     });
 });
