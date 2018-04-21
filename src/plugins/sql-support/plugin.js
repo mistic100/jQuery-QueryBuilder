d3ac6115499d1825b04fd7e723353e8c3668f466
mistic100 <contact@git.strangeplanet.fr>
2018-04-21 12:54:09 +0200

    Fix #676 : IE11 - tokens not replaced in SQL plain output

--- src/plugins/sql-support/plugin.js
+++ src/plugins/sql-support/plugin.js
@@ -328,7 +328,7 @@ QueryBuilder.extend(/** @lends module:plugins.SqlSupport.prototype */ {
                     }
 
                     var sqlFn = function(v) {
-                        return sql.op.replace(/\?/, v);
+                        return sql.op.replace('?', v);
                     };
 
                     /**
