0d41d9c96d848c9ac6abae77cddcf382917f8767
mistic100 <contact@git.strangeplanet.fr>
2018-04-21 13:13:45 +0200

    Fix #681 : Loose MongoDB operator detection (allow additional operators)

--- src/plugins/mongodb-support/plugin.js
+++ src/plugins/mongodb-support/plugin.js
@@ -31,17 +31,17 @@ QueryBuilder.defaults({
     },
 
     mongoRuleOperators: {
+        $eq: function(v) {
+            return {
+                'val': v,
+                'op': v === null ? 'is_null' : (v === '' ? 'is_empty' : 'equal')
+            };
+        },
         $ne: function(v) {
             v = v.$ne;
             return {
                 'val': v,
                 'op': v === null ? 'is_not_null' : (v === '' ? 'is_not_empty' : 'not_equal')
-            };
-        },
-        eq: function(v) {
-            return {
-                'val': v,
-                'op': v === null ? 'is_null' : (v === '' ? 'is_empty' : 'equal')
             };
         },
         $regex: function(v) {
@@ -224,7 +224,7 @@ QueryBuilder.extend(/** @lends module:plugins.MongoDbSupport.prototype */ {
             };
         }
 
-        var key = andOr(query);
+        var key = self.getMongoCondition(query);
         if (!key) {
             Utils.error('MongoParse', 'Invalid MongoDB query format');
         }
@@ -249,7 +249,7 @@ QueryBuilder.extend(/** @lends module:plugins.MongoDbSupport.prototype */ {
                     return;
                 }
 
-                var key = andOr(data);
+                var key = self.getMongoCondition(data);
                 if (key) {
                     parts.push(parse(data, key));
                 }
@@ -257,7 +257,7 @@ QueryBuilder.extend(/** @lends module:plugins.MongoDbSupport.prototype */ {
                     var field = Object.keys(data)[0];
                     var value = data[field];
 
-                    var operator = determineMongoOperator(value, field);
+                    var operator = self.getMongoOperator(value);
                     if (operator === undefined) {
                         Utils.error('MongoParse', 'Invalid MongoDB query format');
                     }
@@ -344,58 +344,50 @@ QueryBuilder.extend(/** @lends module:plugins.MongoDbSupport.prototype */ {
         }
 
         return id;
-    }
-});
+    },
 
-/**
- * Finds which operator is used in a MongoDB sub-object
- * @memberof module:plugins.MongoDbSupport
- * @param {*} value
- * @returns {string|undefined}
- * @private
- */
-function determineMongoOperator(value) {
-    if (value !== null && typeof value == 'object') {
-        var subkeys = Object.keys(value);
-
-        if (subkeys.length === 1) {
-            return subkeys[0];
-        }
-        else {
-            if (value.$gte !== undefined && value.$lte !== undefined) {
+    /**
+     * Finds which operator is used in a MongoDB sub-object
+     * @param {*} data
+     * @returns {string|undefined}
+     * @private
+     */
+    getMongoOperator: function(data) {
+        if (data !== null && typeof data === 'object') {
+            if (data.$gte !== undefined && data.$lte !== undefined) {
                 return 'between';
             }
-            if (value.$lt !== undefined && value.$gt !== undefined) {
+            if (data.$lt !== undefined && data.$gt !== undefined) {
                 return 'not_between';
             }
-            else if (value.$regex !== undefined) { // optional $options
-                return '$regex';
+
+            var knownKeys = Object.keys(data).filter(function(key) {
+                return !!this.settings.mongoRuleOperators[key];
+            }.bind(this));
+
+            if (knownKeys.length === 1) {
+                return knownKeys[0];
             }
-            else {
-                return;
+        }
+        else {
+            return '$eq';
+        }
+    },
+
+
+    /**
+     * Returns the key corresponding to "$or" or "$and"
+     * @param {object} data
+     * @returns {string|undefined}
+     * @private
+     */
+    getMongoCondition: function(data) {
+        var keys = Object.keys(data);
+
+        for (var i = 0, l = keys.length; i < l; i++) {
+            if (keys[i].toLowerCase() === '$or' || keys[i].toLowerCase() === '$and') {
+                return keys[i];
             }
         }
     }
-    else {
-        return 'eq';
-    }
-}
-
-/**
- * Returns the key corresponding to "$or" or "$and"
- * @memberof module:plugins.MongoDbSupport
- * @param {object} data
- * @returns {string}
- * @private
- */
-function andOr(data) {
-    var keys = Object.keys(data);
-
-    for (var i = 0, l = keys.length; i < l; i++) {
-        if (keys[i].toLowerCase() == '$or' || keys[i].toLowerCase() == '$and') {
-            return keys[i];
-        }
-    }
-
-    return undefined;
-}
+});
